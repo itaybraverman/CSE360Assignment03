0bbc447f6551492d3b5e58163ab80faf8d18f647
Itay Braverman <Itay Braverman@SHEBA>
2016-02-21 16:24:00 -0700

    CSE360 Assignment01 part 2d.

---
+++ CSE360_Assignment01/src/cse360assign3/Calculator.java
@@ -0,0 +1,43 @@
+package cse360assign3;
+
+/**
+* Calculator Class
+* Inserts and prints up to 10 integers in an ascending order
+*
+* @author  Itay Braverman PIN# 125
+* @version 1.0
+* @since   2016-01-27
+*/
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+}
